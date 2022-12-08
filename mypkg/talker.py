102c5c9e8f8586a1a1f7185d00e70b7526695e25
shusei Akiya <0912cocacola@gmail.com>
2022-12-08 12:12:48 +0900

    llesson11 2

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -6,19 +6,18 @@ from rclpy.node import Node      #ノードを実装するためのNodeクラス
 from std_msgs.msg import Int16
 
 class Talker():
-    def __init__(self):
+    def __init__(self, node):
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
+        node.create_timer(0.5, self.cb)
+
+def cb(self):
+    msg = Int16()
+    msg.data = self.n
+    self.pub.publish(msg)
+    self.n += 1
 
 rclpy.init()
 node = Node("talker")            #ノード作成（nodeという「オブジェクト」を作成）
-talker = Talker()
-
-def cb():              #関数内のnやpubをtalkerのものに変更
-    msg = Int16()
-    msg.data = talker.n
-    talker.pub.publish(msg)
-    talker.n += 1
-
-node.create_timer(0.5, cb)
+talker = Talker(node)
 rclpy.spin(node)            #実行（無限ループ）
